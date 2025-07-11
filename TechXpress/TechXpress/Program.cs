0a2c80c12e776a97f1c3f9999e9a8129e7482e27
mahmoud <mahmoudelmokaber4@gmail.com>
2025-07-11 04:06:11 +0300

    data base files added

--- TechXpress/TechXpress/Program.cs
+++ TechXpress/TechXpress/Program.cs
@@ -23,7 +23,7 @@ namespace TechXpress
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            // Adding services to the container.
             builder.Services.AddControllersWithViews();
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(builder.Configuration.GetConnectionString("conn")));
